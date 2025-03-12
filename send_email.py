a9290a2b1963abab0223639010a2e1ef456befea
Leonel E. Bracco <leonelatm44@gmail.com>
2025-03-12 13:02:05 +0000

    refactor importante para hacer el curl desde python directamente

--- send_email.py
+++ send_email.py
@@ -7,14 +7,48 @@ from email.mime.base import MIMEBase
 from email import encoders
 from datetime import datetime
 from tabulate import tabulate
-# Load the CSV
+import io
+import subprocess
 
 
-files = os.listdir("./DATA/")
-files.sort(reverse=True)
-file = files[0] #me quedo con el último
-csv_file = f'./DATA/{file}'  # Replace with the path to your CSV file
-df = pd.read_csv(csv_file)
+
+lista_codigos_cargos = ['CCD',
+                        'CCS',
+                        'CYC',
+                        'CYT',
+                        'EAD',
+                        'ECS',
+                        'IAC',
+                        'OCL',
+                        'OCS',
+                        'ODM',
+                        'TCI',
+                        '/PR',
+                        '+3E',
+                        'CCC',
+                        'PEE',
+                        'PRA',
+                        'PRT',
+                        'YCS',
+                        '/3D',
+                        '/3E',
+                        'CMN',
+                        'EDP',
+                        'MKS',
+                        'TLC']
+
+lista_distritos = ['berazategui','quilmes','avellaneda']
+lista_distritos_formateados = "%20".join(lista_distritos)
+
+result = subprocess.run(f'''curl 'https://servicios3.abc.gob.ar/valoracion.docente/api/apd.oferta.encabezado/select?q=*%3A*&rows=10000&facet=true&facet.field=descdistrito&facet.field=descnivelmodalidad&facet.field=cargo&facet.field=estado&facet.limit=20000&facet.mincount=1&json.nl=map&fq=descdistrito%3A{lista_distritos_formateados}&fq=estado:Publicada&wt=csv' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Accept-Language: es-AR,es;q=0.8,en-US;q=0.5,en;q=0.3' --compressed -H 'Connection: keep-alive' -H 'Cookie: _ga=GA1.3.1448501204.1598045085; _gid=GA1.3.1833139648.1598875839; IPCZQX034ee7c6fd=0100c3000a1432ac8ea31b21c67f7acb521691f8; ZNPCQ003-33383400=a89f2a51; ZNPCQ003-32383300=4e5b6dbe' -H 'Upgrade-Insecure-Requests: 1' | iconv -f iso-8859-1 -t UTF-8//TRANSLIT''', shell=True, capture_output=True, text=True)
+df_sin_filtrar = pd.read_csv(io.StringIO(result.stdout))
+df_sin_filtrar["areaincumbencia"] = df_sin_filtrar["areaincumbencia"].str.replace(" ","") #Por las dudas
+df_sin_filtrar["areaincumbencia"] = df_sin_filtrar["areaincumbencia"].str.upper() #Por las dudas
+df = df_sin_filtrar[df_sin_filtrar["areaincumbencia"].isin(lista_codigos_cargos)]
+
+
+
+
 
 # Retrieve sensitive information from GitHub secrets
 smtp_server = os.getenv('SMTP_SERVER', 'smtp.gmail.com')  # Default to Gmail
@@ -22,6 +56,9 @@ smtp_port = os.getenv('SMTP_PORT', '587')  # Default to 587
 sender_email = os.getenv('EMAIL_ADDRESS')  # Your email address
 password = os.getenv('EMAIL_PASSWORD')  # Your email password (or app password)
 receiver_email = os.getenv('EMAIL_SEND') 
+
+
+
 
 
 orden_distritos = {'QUILMES':2,'AVELLANEDA':1,'BERAZATEGUI':0}
